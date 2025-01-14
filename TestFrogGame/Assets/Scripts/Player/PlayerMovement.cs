8b99b2ad4a429dea0c830c1b358b32c314f3d1fc
tvixnottwix <tvishaannem@gmail.com>
2025-01-14 13:39:56 -0500

    More Jump
    The player can infinite jump. Struggling to limit the jump.

--- TestFrogGame/Assets/Scripts/Player/PlayerMovement.cs
+++ TestFrogGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
+    public float jumpForce = 10;
+    private bool isGrounded;
     [SerializeField]  private float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +18,19 @@ public class PlayerMovement : MonoBehaviour
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed,body.linearVelocity.y);
 
         if(Input.GetKey(KeyCode.Space)){
-            body.linearVelocity = new Vector2(body.linearVelocity.x,speed/3);
+            body.linearVelocity = new Vector2(body.linearVelocity.x,jumpForce);
         }
+        // Ground Check
+        void OnCollisionEnter(Collision col) {
+            if (col.gameObject.name == "Ground") {
+                isGrounded = true;
+            }
+        }
+        void OnCollisionExit(Collision col) {
+            if (col.gameObject.name == "Ground") {
+                isGrounded = false;
+            }
+        }
+
     }
 }
